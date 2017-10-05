842a05af5cd53d6483558812a91be5831d90478e
billybob25 <32473001+billybob25@users.noreply.github.com>
2017-10-04 21:02:10 -0700

    Add files via upload

--- src/main/java/com/example/sampath/myapplication/MainActivity.java
+++ src/main/java/com/example/sampath/myapplication/MainActivity.java
@@ -1,27 +1,37 @@
-package com.example.sampath.myapplication;
+package com.example.aasish.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.content.Intent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        Button btnNavToSecond = (Button) findViewById(R.id.button2);
+        Log.d(TAG, "onCreate: Starting");
+
+        Button btnNavToSecond = (Button) findViewById(R.id.button4);
 
         btnNavToSecond.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, Gamecode.class);
-                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, SecondScreen.class);
+                    startActivity(intent);
             }
         });
+
     }
+
 }
